4a396729356796569d18240b52a343c94696c3e6
Petr Skoda <petr@mutms.org>
2025-08-08 10:19:17 +0300

    Prepare mu-4.5.6-01 release

--- version.php
+++ version.php
@@ -30,14 +30,14 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'tool_muprog';
-$plugin->version = 2025080745;
+$plugin->version = 2025080945;
 $plugin->requires = 2024100700;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->supported = [405, 405];
 $plugin->incompatible = 500;
-$plugin->release = 'mu-4.5.5-02+';
+$plugin->release = 'mu-4.5.6-01';
 
 $plugin->dependencies = [
-    'tool_mulib' => 2025080745,
-    'enrol_muprog' => 2025063045,
+    'tool_mulib' => 2025080945,
+    'enrol_muprog' => 2025080945,
 ];
