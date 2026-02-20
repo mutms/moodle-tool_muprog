814d0c3f2183436cb1a18f61804c3663c44617d9
Petr Skoda <petr@mutms.org>
2026-02-20 16:22:03 +0100

    Fix regression

--- management/program_update.php
+++ management/program_update.php
@@ -43,6 +43,7 @@ require_login();
 $program = $DB->get_record('tool_muprog_program', ['id' => $id], '*', MUST_EXIST);
 $context = context::instance_by_id($program->contextid);
 require_capability('tool/muprog:edit', $context);
+$syscontext = \context_system::instance();
 
 $currenturl = new core\url('/admin/tool/muprog/management/program_update.php', ['id' => $program->id]);
 $PAGE->set_context($context);
@@ -53,14 +54,13 @@ $program = file_prepare_standard_editor(
     $program,
     'description',
     $editoroptions,
-    $context,
+    $syscontext,
     'tool_muprog',
     'description',
     $program->id
 );
 $program->tags = core_tag_tag::get_item_tags_array('tool_muprog', 'tool_muprog_program', $program->id);
 
-$syscontext = \context_system::instance();
 $program->image = file_get_submitted_draft_itemid('image');
 file_prepare_draft_area($program->image, $syscontext->id, 'tool_muprog', 'image', $program->id, ['subdirs' => 0]);
 
