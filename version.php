bdcc7125b590807dd838059b5205b711afafb68a
Petr Skoda <petr@mutms.org>
2026-03-01 09:14:55 +0100

    Prepare release

--- version.php
+++ version.php
@@ -30,16 +30,15 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'tool_muprog';
-$plugin->version = 2026022045.02;
+$plugin->version = 2026030145;
 $plugin->requires = 2024100700;
-$plugin->maturity = MATURITY_BETA;
 $plugin->supported = [405, 405];
 $plugin->incompatible = 500;
-$plugin->release = 'v4.5.10.01+';
+$plugin->release = 'v4.5.10.02';
 
 $plugin->dependencies = [
-    'tool_mulib' => 2026021245,
-    'enrol_muprog' => 2026021245,
-    'block_muprog_my' => 2026021245,
-    'block_muprogmyoverview' => 2026022045,
+    'tool_mulib' => 2026030145,
+    'enrol_muprog' => 2026030145,
+    'block_muprog_my' => 2026030145,
+    'block_muprogmyoverview' => 2026030145,
 ];
