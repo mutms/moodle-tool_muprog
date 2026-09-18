54543bc19a483fd2af0bd9ba041265eedaaa0174
Petr Skoda <petr@mutms.org>
2026-09-18 11:49:47 +0200

    Moodle 5.3dev migration

--- version.php
+++ version.php
@@ -30,15 +30,15 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'tool_muprog';
-$plugin->version = 2026091350;
-$plugin->requires = 2025041400;
-$plugin->supported = [500, 502];
+$plugin->version = 2026091953;
+$plugin->requires = 2026091600;
+$plugin->supported = [503, 503];
 
-$plugin->release = 'v5.0.10.01';
+$plugin->release = 'v5.3.0.00';
 
 $plugin->dependencies = [
-    'tool_mulib' => 2026091350,
-    'enrol_muprog' => 2026091350,
-    'block_muprog_my' => 2026091350,
-    'block_muprogmyoverview' => 2026091350,
+    'tool_mulib' => 2026091953,
+    'enrol_muprog' => 2026091953,
+    'block_muprog_my' => 2026091953,
+    'block_muprogmyoverview' => 2026091953,
 ];
